ad9c50c8417df995e49d981534ff4631cd005287
winniehell <git@winniehell.de>
2025-06-11 09:10:36 +0200

    Add build script for browser-chat

---
+++ browser-chat/build-on-nixos.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+project_dir=$(cd "$(dirname "$0")" && pwd)
+
+cd "${project_dir}/frontend/" || false
+
+# gcc is default
+clang_env="CC_wasm32_unknown_unknown=clang"
+
+# include path to standard library is missing by default
+clang_path=$(nix build --no-link --print-out-paths nixpkgs#llvmPackages.clang)
+clang_env="${clang_env} CFLAGS_wasm32_unknown_unknown='-I${clang_path}/resource-root/include/'"
+
+nix-shell \
+  --run "export ${clang_env} && npm run build:wasm" \
+  --packages llvmPackages.clang-unwrapped wasm-pack
+
+npm install
